6d51616bd6c6ec0ae0ba3f61d5114d612d7242ea
liadk07 <91607695+liad07@users.noreply.github.com>
2022-01-12 21:00:27 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var link = "www.themarker.com/misc/themarkersmartphoneapp/.premium-1";
+var num;
+console.log("run the first time for tutorial");
+const input = prompt("run the first time for tutorial",num);
+let a ;
+var linkf=link+input;
+
+let text = "unlock link";
+let result = text.link(link+input);
+let text1 = "the marker";
+let result1 = text1.link("https://www.themarker.com/");
+
+document.writeln(result)
+    document.writeln(result1)
+    document.writeln('<a href="README.txt" <button  class="btn"><i class="fa fa-download"></i> Download</button>')
+    document.write('<img src="pic.png">');
+
+
+
